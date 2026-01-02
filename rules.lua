50c0dace72ff5f02febc2e01aeb76f9d5773596c
MarcoSon <aaa226898374@gmail.com>
2026-01-03 03:33:01 +0800

    Update rules.lua

--- rules.lua
+++ rules.lua
@@ -1,8 +1,15 @@
+-- rules.lua (監控電腦)
 dofile("actions.lua")
+
 rules = {
   {
     item = "minecraft:iron_ingot",
     low  = 256,
-    act  = actions.pulse("right", 0.2),
+    act  = actions.startRemote(12, "main"),
+  },
+  {
+    item = "minecraft:coal",
+    low  = 128,
+    act  = actions.startRemote(23, "main"),
   },
 }
