c0855ef349949a6661c8f7f0855f9ee64851fd49
MarcoSon <aaa226898374@gmail.com>
2026-01-03 17:04:56 +0800

    Update InstallReceiver.lua

--- Receiver/InstallReceiver.lua
+++ Receiver/InstallReceiver.lua
@@ -1,22 +1,25 @@
 local base = "https://raw.githubusercontent.com/aaa226898374-stack/testforCCLUN/main/"
 local profile = ...
 
+
 if not profile then
   print("Usage: InstallReceiver <RuleName>")
-  print("Example: InstallReceiver RuleForPress1")
+  print("Example: InstallReceiver RulePress1")
   return
 end
 
-local files = { "Receiver/ReceiverMain.lua", "Receiver/UpdateReceiverRule.lua", "Receiver/startup.lua" }
-for _, f in ipairs(files) do
-  shell.run("wget", base .. f, fs.getName(f))
-end
+if not fs.exists("Rules") then fs.makeDir("Rules") end
+
+shell.run("wget", base .. "Receiver/ReceiverMain.lua", "ReceiverMain.lua")
+shell.run("wget", base .. "Receiver/startup.lua", "startup.lua")
+
+
+shell.run("wget", base .. "Rules/" .. profile .. ".lua", "Rules/" .. profile .. ".lua")
 
 local cfgContent = string.format([[
 return {
   RuleProfile = "%s",
   Net = { Protocol = "stock" },
-  Self = { ModemSide = "top" },
 }
 ]], profile)
 
@@ -24,14 +27,5 @@ local h = fs.open("ReceiverConfig.lua", "w")
 h.write(cfgContent)
 h.close()
 
-
-if not fs.exists("ReceiverRules") then fs.makeDir("ReceiverRules") end
-shell.run("wget", base .. "ReceiverRules/" .. profile .. ".lua", "ReceiverRules/" .. profile .. ".lua")
-
-
-print("--------------------------------")
-print("Install Complete!")
-print("THIS COMPUTER ID: " .. os.getComputerID()) 
-print("--------------------------------")
-print("Please update MonitorRules.lua with ID: " .. os.getComputerID())
+print("ID: " .. os.getComputerID())
 print("Reboot to start.")
