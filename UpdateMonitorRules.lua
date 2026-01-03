aef9e2fe4c4596cb0579d5965eae9b5f908c79c4
MarcoSon <aaa226898374@gmail.com>
2026-01-03 16:03:02 +0800

    Update UpdateMonitorRules.lua

--- UpdateMonitorRules.lua
+++ UpdateMonitorRules.lua
@@ -1,1 +1,7 @@
+local base = "https://raw.githubusercontent.com/aaa226898374-stack/testforCCLUN/main/"
 
+if fs.exists("MonitorRules.lua") then fs.delete("MonitorRules.lua") end
+shell.run("wget", base .. "Monitor/MonitorRules.lua", "MonitorRules.lua")
+
+print("MonitorRules updated")
+print("Restart MonitorMain to apply")
