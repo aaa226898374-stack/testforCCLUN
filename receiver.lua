c0db77db644582a0af89e7002123ea18a29b1881
MarcoSon <aaa226898374@gmail.com>
2026-01-03 15:54:23 +0800

    Delete receiver.lua

--- receiver.lua
+++
@@ -1,8 +0,0 @@
-rednet.open("back")
-
-while true do
-  local from, msg = rednet.receive("stock")
-  if type(msg) == "table" and msg.cmd == "start" and type(msg.program) == "string" then
-    shell.run(msg.program)
-  end
-end
