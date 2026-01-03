c7feba3ba455344c6f7a4636c5b70e190190e870
MarcoSon <aaa226898374@gmail.com>
2026-01-03 16:11:51 +0800

    Update MonitorRules.lua

--- Monitor/MonitorRules.lua
+++ Monitor/MonitorRules.lua
@@ -1,1 +1,24 @@
+return {
+  Net = { Protocol = "stock" },
+  Self = { ModemSide = "back" },
+  Scan = { Prefix = "functionalstorage:", Interval = 1 },
+
+  GetRules = {
+    {
+      Resource = "ftbmaterials:copper_plate",
+      Low = 256,
+      TargetHost = "press1",
+    },
+    {
+      Resource = "ftbmaterials:iron_plate",
+      Low = 256,
+      TargetHost = "press1",
+    },
+    {
+      Resource = "minecraft:coal",
+      Low = 128,
+      TargetHost = "furnace1",
+    },
+  }
+}
 
