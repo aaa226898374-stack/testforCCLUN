7557c6f571c6052537ae9e190b4a3e0c7af7e537
MarcoSon <aaa226898374@gmail.com>
2026-01-03 15:15:43 +0800

    Update actions.lua

--- actions.lua
+++ actions.lua
@@ -1,16 +1,7 @@
 actions = {}
 
-function actions.pulse(side, sec)
-  sec = sec or 0.2
+function actions.startRemote(targetId, program)
   return function()
-    redstone.setOutput(side, true)
-    sleep(sec)
-    redstone.setOutput(side, false)
-  end
-end
-
-function actions.set(side, level)
-  return function()
-    redstone.setOutput(side, level and true or false)
+    rednet.send(targetId, { cmd = "start", program = program }, "stock")
   end
 end
