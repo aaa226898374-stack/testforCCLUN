988a7aa1cc275bec38c8f9545769838da9f443df
MarcoSon <aaa226898374@gmail.com>
2026-01-03 17:09:39 +0800

    Create RuleMonitor.lua

---
+++ Rules/RuleMonitor.lua
@@ -0,0 +1,17 @@
+return {
+  Net = { Protocol = "stock" },
+  Scan = { Prefix = "functionalstorage:", Interval = 2 },
+
+  GetRules = {
+    {
+      Resource = "ftbmaterials:copper_plate",
+      Low = 256,
+      TargetID = 15,
+    },
+    {
+      Resource = "minecraft:coal",
+      Low = 128,
+      TargetID = 16,
+    },
+  }
+}
