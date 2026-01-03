a54e82fc2e41437553c7fb3efb58927ae63ea7f3
MarcoSon <aaa226898374@gmail.com>
2026-01-03 16:14:08 +0800

    Update RuleForPress1.lua

--- ReceiverRules/RuleForPress1.lua
+++ ReceiverRules/RuleForPress1.lua
@@ -1,1 +1,11 @@
+return {
+  PutRules = {
+    ["ftbmaterials:copper_plate"] = {
+      Outputs = { { Side = "left" } }
+    },
+    ["ftbmaterials:iron_plate"] = {
+      Outputs = { { Side = "right" } }
+    },
+  }
+}
 
