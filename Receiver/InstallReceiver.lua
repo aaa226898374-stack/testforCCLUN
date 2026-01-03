429444940d7a4f7a0d9dd142b2e6b0a9e98da0fa
MarcoSon <aaa226898374@gmail.com>
2026-01-03 17:07:55 +0800

    Update InstallReceiver.lua

--- Receiver/InstallReceiver.lua
+++ Receiver/InstallReceiver.lua
@@ -1,20 +1,23 @@
 local base = "https://raw.githubusercontent.com/aaa226898374-stack/testforCCLUN/main/"
 local profile = ...
 
-
 if not profile then
   print("Usage: InstallReceiver <RuleName>")
-  print("Example: InstallReceiver RulePress1")
   return
 end
 
 if not fs.exists("Rules") then fs.makeDir("Rules") end
+if not fs.exists("Tools") then fs.makeDir("Tools") end
 
 shell.run("wget", base .. "Receiver/ReceiverMain.lua", "ReceiverMain.lua")
 shell.run("wget", base .. "Receiver/startup.lua", "startup.lua")
-
-
 shell.run("wget", base .. "Rules/" .. profile .. ".lua", "Rules/" .. profile .. ".lua")
+
+local tools = {"reset.lua", "id.lua"}
+for _, t in ipairs(tools) do
+    shell.run("wget", base .. "Tools/" .. t, "Tools/" .. t)
+end
+fs.copy("Tools/reset.lua", "reset")
 
 local cfgContent = string.format([[
 return {
