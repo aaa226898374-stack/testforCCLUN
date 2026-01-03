e70f6f775a70e4d389018d17ae6117677a110165
MarcoSon <aaa226898374@gmail.com>
2026-01-03 17:07:00 +0800

    Update InstallMonitor.lua

--- Monitor/InstallMonitor.lua
+++ Monitor/InstallMonitor.lua
@@ -1,11 +1,16 @@
 local base = "https://raw.githubusercontent.com/aaa226898374-stack/testforCCLUN/main/"
 
 if not fs.exists("Rules") then fs.makeDir("Rules") end
+if not fs.exists("Tools") then fs.makeDir("Tools") end
 
 shell.run("wget", base .. "Monitor/MonitorMain.lua", "MonitorMain.lua")
-
 shell.run("wget", base .. "Rules/RuleMonitor.lua", "Rules/RuleMonitor.lua")
 
+local tools = {"reset.lua", "scan.lua", "id.lua"}
+for _, t in ipairs(tools) do
+    shell.run("wget", base .. "Tools/" .. t, "Tools/" .. t)
+end
+fs.copy("Tools/reset.lua", "reset")
+
 print("Monitor Installed.")
-print("Config: Rules/RuleMonitor.lua")
 print("Run: MonitorMain")
